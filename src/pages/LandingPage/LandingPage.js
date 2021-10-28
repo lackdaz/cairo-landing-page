298f2d651bd6a584b6d644df1f1a73e96f0fb0aa
Seth Loh <lackdaz@gmail.com>
2021-10-28 20:42:01 +0800

    Revert "changed header (#61)"
    This reverts commit 4b684b676a9e9ebe060832463450a0f894a29ebe.

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -1,48 +1,40 @@
-import React from "react";
-import PropTypes from "prop-types";
-import SocialIcons from "@components/SocialIcons";
-import Nav from "@components/Nav";
-import ScrollToNext from "@components/ScrollToNext";
-import BrowserNotes from "@components/BrowserNotes";
-import logo from "@images/cairo-flying.png";
+import React from 'react';
+import PropTypes from 'prop-types';
+import SocialIcons from '@components/SocialIcons';
+import Nav from '@components/Nav';
+import ScrollToNext from '@components/ScrollToNext';
+import BrowserNotes from '@components/BrowserNotes';
+import logo from '@images/cairo-flying.png';
 
-import "./style.scss";
+import './style.scss';
 
 const LandingPage = (props, context) => {
-    const {
-        theme: { bgPrimary, colorPrimary },
-    } = context;
+  const { theme: { bgPrimary, colorPrimary } } = context;
 
-    return (
-        <div style={{ backgroundColor: bgPrimary }} className="landing-page">
-            <Nav />
-            <main style={{ color: colorPrimary }}>
-                <BrowserNotes />
-                <div className="intro-wrapper">
-                    <div className="intro-name">Hello, I'm Cairo!</div>
-                    <div className="tagline">
-                        (pronounced{" "}
-                        <b>
-                            <i>chai-row</i>
-                        </b>
-                        , or <b>菜肉</b>)
-                    </div>
-                    <div className="logo">
-                        <img src={logo} alt="cairo stylized" />
-                    </div>
-                    <div className="tagline">
-                        Headgroomer | Sweet Talker | Escapologist
-                    </div>
-                    <SocialIcons />
-                </div>
-            </main>
-            <ScrollToNext pageSelector=".about-page" />
+  return (
+    <div style={{ backgroundColor: bgPrimary }} className="landing-page">
+      <Nav />
+      <main style={{ color: colorPrimary }}>
+        <BrowserNotes />
+        <div className="intro-wrapper">
+          <div className="intro-name">Hello, I'm Cairo!</div>
+          <div className="tagline">(pronounced <b><i>chai-row</i></b>, or <b>菜肉</b>)</div>
+          <div className="logo">
+            <img src={logo} alt="cairo stylized"/>
+          </div>
+          <div className="tagline">
+            Angsty Teenager | Sweet Talker | Escapologist
+          </div>
+          <SocialIcons />
         </div>
-    );
+      </main>
+      <ScrollToNext pageSelector=".about-page" />
+    </div>
+  );
 };
 
 LandingPage.contextTypes = {
-    theme: PropTypes.any,
+  theme: PropTypes.any
 };
 
 export default LandingPage;
